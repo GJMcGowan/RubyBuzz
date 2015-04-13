e3461bc40bee6e08405f1af1662e94dfdfebec26
GJMcGowan <gemcgow@gmail.com>
2015-04-13 18:51:20 +0100

    Minor changes, slightly less code

--- lib/rubybuzz.rb
+++ lib/rubybuzz.rb
@@ -1,19 +1,10 @@
 # rubocop
 class Rubybuzz
-  def both?(number)
-    div_by(number, 3) && div_by(number, 5)
-  end
-
-  def none?(number)
-    both?(number) || div_by(number, 3) || div_by(number, 5)
-  end
-
   def say(number)
-    item = 'Fizz' if div_by(number, 3)
-    item = 'Buzz' if div_by(number, 5)
-    item = 'FizzBuzz' if both?(number)
-    item = number unless none?(number)
-    item
+    return 'FizzBuzz' if both?(number)
+    return 'Fizz' if div_by(number, 3)
+    return 'Buzz' if div_by(number, 5)
+    return number unless none?(number)
   end
 
   def play(max)
@@ -26,5 +17,13 @@ class Rubybuzz
 
   def div_by(number, divisor)
     number % divisor == 0
+  end
+
+  def both?(number)
+    div_by(number, 3) && div_by(number, 5)
+  end
+
+  def none?(number)
+    both?(number) || div_by(number, 3) || div_by(number, 5)
   end
 end
